c316de0fe52b7a5606e724403e777f40a2b6a577
Francis <francismlee@hotmail.com>
2023-12-20 03:02:52 -0500

    Prototype frontend. Missing search button.

---
+++ app/javascript/home.js
@@ -0,0 +1,6 @@
+import 'materialize-css/dist/js/materialize';
+
+document.addEventListener('DOMContentLoaded', function() //initiate Materialize once everything is loaded.
+{ 
+    M.AutoInit();
+});
